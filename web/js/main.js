d576c131ea0abcfc498e95549e9a1fbdc4841391
Rob Dawson <rob@codebox.org.uk>
2020-08-20 21:17:13 +0100

    image pieces

--- web/js/main.js
+++ web/js/main.js
@@ -1,18 +1,59 @@
 const img = new Image();
 
-function makeImage(imageData) {
+
+function buildImageFactory(imageData) {
     "use strict";
     const data = imageData.data,
         width = imageData.width;
-    return {
-        getPixel(x,y) {
-            const redIndex = (width * 4 * y) + (x * 4),
-                greenIndex = redIndex + 1,
-                blueIndex = greenIndex + 1;
-            return [data[redIndex], data[greenIndex], data[blueIndex]];
+
+    function getPixel(x,y) {
+        const redIndex = (width * 4 * y) + (x * 4),
+            greenIndex = redIndex + 1,
+            blueIndex = greenIndex + 1;
+        return [data[redIndex], data[greenIndex], data[blueIndex]];
+    }
+
+    const factory = {
+        makeImagePiece(x1, y1, x2, y2) {
+            function forEach(fn) {
+                for (let x = x1; x <= x2; x++) {
+                    for (let y = y1; y <= y2; y++) {
+                        fn(getPixel(x,y));
+                    }
+                }
+            }
+            return {
+                getVariance() {
+                    let min = Number.MAX_VALUE,
+                        max = Number.MIN_VALUE;
+                    forEach(p => {
+                        const v = p[0] + p[1] + p[2];
+                        min = Math.min(min, v);
+                        max = Math.max(max, v);
+                    });
+                    return max - min;
+                },
+                split() {
+                    const xDiff = x2 - x1,
+                        yDiff = y2 - y1;
+                    if (xDiff < yDiff) {
+                        const ySplit = Math.round((y1 + y2) / 2),
+                            topPiece = factory.makeImagePiece(x1, y1, x2, ySplit),
+                            bottomPiece = factory.makeImagePiece(x1, ySplit+1, x2, y2);
+                        return [topPiece , bottomPiece];
+                    } else {
+                        const xSplit = Math.round((x1 + x2) / 2),
+                            leftPiece = factory.makeImagePiece(x1, y1, xSplit, y2),
+                            rightPiece = factory.makeImagePiece(xSplit+1, y1, x2, y2);
+                        return [leftPiece , rightPiece];
+                    }
+                }
+            };
         }
     };
+    return factory;
 }
+
 img.onload = () => {
     const canvas = document.createElement('canvas');
     canvas.width = img.width;
@@ -23,4 +64,17 @@ img.onload = () => {
         image = makeImage(imageData);
 };
 img.src = './test.png';
+
+const VARIANCE_THRESHOLD = 10, queue = [], finished = [];
+
+while (queue.length) {
+    const nextPiece = queue.shift();
+    if (nextPiece.getVariance() < VARIANCE_THRESHOLD) {
+        finished.push(nextPiece);
+    } else {
+        const [piece1, piece2] = nextPiece.split();
+        queue.push(piece1);
+        queue.push(piece2);
+    }
+}
 
