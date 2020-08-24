b9a3c3b3aae5ea14a1f7a80e451cba0d07a535c7
Rob Dawson <rob@codebox.org.uk>
2020-08-24 21:14:59 +0100

    polygons need sorting

--- python/main.py
+++ python/main.py
@@ -1,14 +1,16 @@
-import sys, os.path, cv2
+import sys, os.path, cv2, math, colorsys
 import numpy as np
 from PIL import Image, ImageDraw
 
 IMAGE_BACKGROUND = (255, 255, 255)
 VARIANCE_THRESHOLD = 200
 MIN_PIECE_SIZE = 5
-OUTPUT_SCALE = 2
+OUTPUT_SCALE = 4
 BLOCK_HEIGHT_FACTOR = 1000
 MAX_HEIGHT = 50
 VIEW_DISTANCE = 1000
+TILT = math.pi / 4;
+
 
 class ImagePiece:
     def __init__(self, image, x1, y1, x2, y2):
@@ -21,19 +23,19 @@ class ImagePiece:
         self.width = x2 - x1
         self.height = y2 - y1
         self.variance = None
-        self.colour = None
+        self.rgb_colour = None
 
         full_image_width, full_image_height = image.size
         self.area_fraction = (self.width * self.height) / (full_image_width * full_image_height)
 
     def split(self):
         if (self.width < self.height):
-            y_split = int((self.y1 + self.y2) / 2)
+            y_split = int(round((self.y1 + self.y2) / 2))
             top_piece = ImagePiece(self.image, self.x1, self.y1, self.x2, y_split)
             bottom_piece = ImagePiece(self.image, self.x1, y_split+1, self.x2, self.y2)
             return top_piece, bottom_piece
         else:
-            x_split = int((self.x1 + self.x2) / 2)
+            x_split = int(round((self.x1 + self.x2) / 2))
             left_piece = ImagePiece(self.image, self.x1, self.y1, x_split, self.y2)
             right_piece = ImagePiece(self.image, x_split+1, self.y1, self.x2, self.y2)
             return left_piece, right_piece
@@ -57,8 +59,8 @@ class ImagePiece:
 
         return self.variance
 
-    def get_colour(self):
-        if self.colour is None:
+    def get_rgb_colour(self):
+        if self.rgb_colour is None:
             r_total = g_total = b_total = count = 0
 
             for x in range(self.x1, self.x2+1):
@@ -69,9 +71,9 @@ class ImagePiece:
                     b_total += b
                     count += 1
 
-            self.colour = (int(r_total/count), int(g_total/count), int(b_total/count))
+            self.rgb_colour = (int(r_total/count), int(g_total/count), int(b_total/count))
 
-        return self.colour
+        return self.rgb_colour
 
 class ImagePieceBuilder:
     def __init__(self, image):
@@ -86,13 +88,22 @@ class CoordTransformer:
         self.view_height = view_height
         self.view_distance = view_distance
 
+    def __tilt(self, x, y, z):
+        return {
+            'x': x,
+            'y': (z-VIEW_DISTANCE) * math.sin(TILT) + (y + self.view_height/2) * math.cos(TILT) - self.view_height/2 ,
+            'z': (z-VIEW_DISTANCE) * math.cos(TILT) - (y + self.view_height/2) * math.sin(TILT) + VIEW_DISTANCE
+        }
+
     def transform(self, x, y, height):
         x3d_flat = x - self.view_width/2
         y3d_flat = height - self.view_height/2
         z3d_flat = VIEW_DISTANCE + self.view_height - y
 
-        transformed_x = x3d_flat * VIEW_DISTANCE / z3d_flat
-        transformed_y  = y3d_flat * VIEW_DISTANCE / z3d_flat
+        tilted_coords = self.__tilt(x3d_flat, y3d_flat, z3d_flat)
+
+        transformed_x = tilted_coords['x'] * VIEW_DISTANCE / tilted_coords['z']
+        transformed_y = tilted_coords['y'] * VIEW_DISTANCE / tilted_coords['z']
 
         return {'x': transformed_x + self.view_width / 2, 'y': -transformed_y + self.view_height / 2}
 
@@ -100,7 +111,6 @@ class CoordTransformer:
 class PieceRenderer:
     def __init__(self, image):
         self.image = image
-        self.image_draw = ImageDraw.Draw(image)
 
         image_width, image_height = image.size
         self.transformer = CoordTransformer(image_width, image_height, VIEW_DISTANCE)
@@ -109,11 +119,30 @@ class PieceRenderer:
     def __point(self, p):
         return p['x'], p['y']
 
-    def draw(self, image_piece):
+    def __to_hsl(self, rgb_colour, lightness_change):
+        h, l, s = colorsys.rgb_to_hls(rgb_colour[0]/255, rgb_colour[1]/255, rgb_colour[2]/255)
+        return 'hsl({},{}%,{}%)'.format(int(h * 360), int(s * 100), min(int(l * lightness_change * 100), 100))
+
+    def __colour_top_face(self, top_colour):
+        return self.__to_hsl(top_colour, 1)
+
+    def __colour_front_face(self, top_colour):
+        return self.__to_hsl(top_colour, 0.8)
+
+    def __colour_back_face(self, top_colour):
+        return self.__to_hsl(top_colour, 1.2)
+
+    def __colour_left_face(self, top_colour):
+        return self.__to_hsl(top_colour, 1.4)
+
+    def __colour_right_face(self, top_colour):
+        return self.__to_hsl(top_colour, 0.6)
+
+    def get_polygons(self, image_piece):
         x1 = image_piece.x1 * OUTPUT_SCALE
         y1 = image_piece.y1 * OUTPUT_SCALE
-        x2 = image_piece.x2 * OUTPUT_SCALE
-        y2 = image_piece.y2 * OUTPUT_SCALE
+        x2 = (image_piece.x2 + 1) * OUTPUT_SCALE - 1
+        y2 = (image_piece.y2 + 1) * OUTPUT_SCALE - 1
         h1 = min(image_piece.area_fraction * BLOCK_HEIGHT_FACTOR, MAX_HEIGHT) * OUTPUT_SCALE
         h2 = 0
 
@@ -127,39 +156,20 @@ class PieceRenderer:
         bfl = self.transformer.transform(x1, y2, h2)
         bfr = self.transformer.transform(x2, y2, h2)
 
-        # Top face
-        self.image_draw.polygon([
-            self.__point(tbl),
-            self.__point(tbr),
-            self.__point(tfr),
-            self.__point(tfl)
-        ],fill=image_piece.get_colour())
+        rgb_colour = image_piece.get_rgb_colour()
 
-        # Front face
-        self.image_draw.polygon([
-            self.__point(tfl),
-            self.__point(tfr),
-            self.__point(bfr),
-            self.__point(bfl)
-        ],fill=image_piece.get_colour())
-
-        # Right face
-        if tfr['x'] < self.image_width/2:
-            self.image_draw.polygon([
-                self.__point(tbr),
-                self.__point(bbr),
-                self.__point(bfr),
-                self.__point(tfr)
-            ],fill=image_piece.get_colour())
-
-        # Left face
-        if tfl['x'] > self.image_width/2:
-            self.image_draw.polygon([
-                self.__point(tbl),
-                self.__point(bbl),
-                self.__point(bfl),
-                self.__point(tfl)
-            ],fill=image_piece.get_colour())
+        return [
+            # Top face
+            (self.__point(tbl), self.__point(tbr), self.__point(tfr), self.__point(tfl), self.__colour_top_face(rgb_colour)),
+            # Front face
+            (self.__point(tfl), self.__point(tfr), self.__point(bfr), self.__point(bfl), self.__colour_front_face(rgb_colour)),
+            # Back face
+            (self.__point(tbl), self.__point(tbr), self.__point(bbr), self.__point(bbl), self.__colour_back_face(rgb_colour)),
+            # Right face
+            (self.__point(tbr), self.__point(bbr), self.__point(bfr), self.__point(tfr), self.__colour_right_face(rgb_colour)),
+            # Left face
+            (self.__point(tbl), self.__point(bbl), self.__point(bfl), self.__point(tfl), self.__colour_left_face(rgb_colour)),
+        ]
 
 def transform_frame(frame_in, width_in, height_in):
     img_in = Image.fromarray(frame_in)
@@ -181,7 +191,15 @@ def transform_frame(frame_in, width_in, height_in):
         allocate_to_queue(half_piece_2)
 
     renderer = PieceRenderer(img_out)
-    [renderer.draw(piece) for piece in finished_queue]
+    polygons = [polygon for piece in finished_queue for polygon in renderer.get_polygons(piece)]
+
+    def get_polygon_distance(p):
+        return 1
+
+    polygons.sort(key=get_polygon_distance)
+
+    image_draw = ImageDraw.Draw(img_out)
+    [image_draw.polygon([p[0], p[1], p[2], p[3]], fill=p[4]) for p in polygons]
 
     frame_out = np.asarray(img_out)
 
